b0301a278901c31d925d0b541a7e329d49a9d289
zeus <33592162+1nam@users.noreply.github.com>
2020-09-03 01:56:14 -0400

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get upgrade
+sudo apt-get autoclean
+sudo apt-get clean
+sudo apt-get autoremove
+notify-send " Finished Enjoy! :)"
+exit
